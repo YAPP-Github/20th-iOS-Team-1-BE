a4b2d6068715556b879068872ca0f635b60ae541
jihoon <znftm97@naver.com>
2022-05-23 20:55:25 +0900

    feat: add Healthcheck API
    - Healthcheck API 추가
    - Secuirty 기본 설정 추가

---
+++ src/main/java/com/yapp/pet/global/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.yapp.pet.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/swagger/**", "/swagger-ui/**", "/swagger-ui.html/**", "/swagger-resources/**").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/favicon.ico","/error", "/swagger-ui/**", "/v3/api-docs/**");
+    }
+}
+
