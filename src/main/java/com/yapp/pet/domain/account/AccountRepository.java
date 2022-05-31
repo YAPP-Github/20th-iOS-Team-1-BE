1db94401475d185cca9f4161f0a0bf2b3b584b98
jihoon <znftm97@naver.com>
2022-05-31 23:20:47 +0900

    refactor: 커스텀 예외로 변경
    - 제공해주는 Exception 사용 시 매번 핸들러 추가 필요로 인해
    커스텀 예외 사용

--- src/main/java/com/yapp/pet/domain/account/AccountRepository.java
+++ src/main/java/com/yapp/pet/domain/account/AccountRepository.java
@@ -1,13 +1,12 @@
 package com.yapp.pet.domain.account;
 
 import com.yapp.pet.domain.account.entity.Account;
+import com.yapp.pet.global.exception.Account.AccountNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import javax.persistence.EntityNotFoundException;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     default Account findByIdWrapper(Long accountId){
-        return findById(accountId).orElseThrow(EntityNotFoundException::new);
+        return findById(accountId).orElseThrow(AccountNotFoundException::new);
     }
 }
