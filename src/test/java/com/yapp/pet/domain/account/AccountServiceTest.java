550809d13bdc42e3e79c644ae628aa872f7871d0
jihoon <znftm97@naver.com>
2022-06-09 00:24:12 +0900

    test: 이미지 포함 회원가입 테스트 코드 추가
    - data.sql 파일 수정
    - 테스트용 이미지 추가

--- src/test/java/com/yapp/pet/domain/account/AccountServiceTest.java
+++ src/test/java/com/yapp/pet/domain/account/AccountServiceTest.java
@@ -3,6 +3,7 @@ package com.yapp.pet.domain.account;
 import com.yapp.pet.domain.account.entity.Account;
 import com.yapp.pet.domain.account.entity.AccountSex;
 import com.yapp.pet.domain.account.repository.AccountRepository;
+import com.yapp.pet.domain.account_image.AccountImageRepository;
 import com.yapp.pet.domain.token.entity.Social;
 import com.yapp.pet.domain.token.repository.TokenRepository;
 import com.yapp.pet.global.jwt.JwtService;
@@ -20,11 +21,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.security.Key;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static com.yapp.pet.global.TogaetherConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -45,6 +54,9 @@ public class AccountServiceTest {
 
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    AccountImageRepository accountImageRepository;
 
     @Value("${jwt.token.secret}")
     String secret;
@@ -81,8 +93,29 @@ public class AccountServiceTest {
                 .compact();
     }
 
+    List<MultipartFile> createMockimageFiles() throws IOException {
+        List<MultipartFile> mockFiles = new ArrayList<>();
+
+        MockMultipartFile mockMultipartFile1 = new MockMultipartFile(
+                "mockImage1",
+                "cat",
+                MediaType.IMAGE_JPEG_VALUE,
+                new FileInputStream(new File("src/test/resources/assets/cat.jpg")));
+
+        MockMultipartFile mockMultipartFile2 = new MockMultipartFile(
+                "mockImage2",
+                "city",
+                MediaType.IMAGE_JPEG_VALUE,
+                new FileInputStream(new File("src/test/resources/assets/city.jpg")));
+
+        mockFiles.add(mockMultipartFile1);
+        mockFiles.add(mockMultipartFile2);
+
+        return mockFiles;
+    }
+
     @Test
-    @DisplayName("회원가입 - 첫 서비스 접근 시 Account 저장")
+    @DisplayName("[회원가입 - 첫 서비스 접근 시 Account 저장")
     void signUp(){
         //given
         String idToken = createIdToken("newUniqueId");
@@ -97,8 +130,8 @@ public class AccountServiceTest {
     }
 
     @Test
-    @DisplayName("회원가입 - 추가 정보 입력")
-    void signUpAddInfo(){
+    @DisplayName("회원가입 - 추가 정보 입력 (이미지 없음)")
+    void signUpAddInfo() {
         //given
         AccountSignUpRequest req = new AccountSignUpRequest();
 
@@ -109,7 +142,7 @@ public class AccountServiceTest {
         req.setSex(AccountSex.MAN);
 
         //when
-        Long accountId = accountService.signUp(accountWithoutToken, req);
+        Long accountId = accountService.signUp(accountWithoutToken, req, null);
 
         //then
         assertThat(accountId).isEqualTo(accountWithoutToken.getId());
@@ -118,6 +151,33 @@ public class AccountServiceTest {
         assertThat(req.getDetail()).isEqualTo(accountWithoutToken.getAddress().getDetail());
         assertThat(req.getNickname()).isEqualTo(accountWithoutToken.getNickname());
         assertThat(req.getSex()).isEqualTo(accountWithoutToken.getSex());
+    }
+
+    @Test
+    @DisplayName("회원가입 - 추가 정보 입력 (이미지 포함)")
+    void signUpAddInfoWithImage() throws IOException {
+        //given
+        AccountSignUpRequest req = new AccountSignUpRequest();
+        req.setAge(25);
+        req.setCity("서울시 강남구");
+        req.setDetail("테헤란로 910");
+        req.setNickname("nick");
+        req.setSex(AccountSex.MAN);
+
+        List<MultipartFile> imageFiles = createMockimageFiles();
+
+        //when
+        Long accountId = accountService.signUp(accountWithoutToken, req, imageFiles.get(0));
+
+        //then
+        assertThat(accountId).isEqualTo(accountWithoutToken.getId());
+        assertThat(req.getAge()).isEqualTo(accountWithoutToken.getAge());
+        assertThat(req.getCity()).isEqualTo(accountWithoutToken.getAddress().getCity());
+        assertThat(req.getDetail()).isEqualTo(accountWithoutToken.getAddress().getDetail());
+        assertThat(req.getNickname()).isEqualTo(accountWithoutToken.getNickname());
+        assertThat(req.getSex()).isEqualTo(accountWithoutToken.getSex());
+
+        assertThat(accountImageRepository.findById(1L).isEmpty()).isFalse();
     }
 
     @Test
@@ -138,7 +198,7 @@ public class AccountServiceTest {
     }
 
     @Test
-    @DisplayName("[성공] 닉네임 검증 로직 통과 - 중복되지 않고 2글자~10글자인 닉네임")
+    @DisplayName("닉네임 검증 로직 통과 - 중복되지 않고 2글자~10글자인 닉네임")
     void successValidateNickname(){
         //given
         String nickname = "투개더";
