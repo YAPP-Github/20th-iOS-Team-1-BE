7f6dbd1d935d507e0443d6607f8ee89aebae4c41
jihoon <znftm97@naver.com>
2022-06-09 01:19:23 +0900

    chore: 패키지명 수정

--- src/main/java/com/yapp/pet/domain/account/repository/AccountRepository.java
+++ src/main/java/com/yapp/pet/domain/account/repository/AccountRepository.java
@@ -1,7 +1,7 @@
 package com.yapp.pet.domain.account.repository;
 
 import com.yapp.pet.domain.account.entity.Account;
-import com.yapp.pet.global.exception.Account.AccountNotFoundException;
+import com.yapp.pet.global.exception.account.AccountNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long>, AccountRepositoryCustom {
