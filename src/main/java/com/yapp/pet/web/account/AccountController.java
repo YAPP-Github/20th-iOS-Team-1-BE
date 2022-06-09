06a873be509db1356176a6770fcb4c51ff6b4053
jihoon <znftm97@naver.com>
2022-06-09 21:03:45 +0900

    refactor: Swagger annotation 제거

--- src/main/java/com/yapp/pet/web/account/AccountController.java
+++ src/main/java/com/yapp/pet/web/account/AccountController.java
@@ -21,18 +21,11 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-@Tag(name = "계정", description = "계정 관련 API를 제공합니다.")
 public class AccountController {
 
     private final AccountService accountService;
 
     @GetMapping("/accounts/validate-nickname")
-    @Operation(summary = "닉네임 중복 체크", tags = "계정",
-            description = "닉네임이 중복되면 true , 중복되지 않으면 false 값을 리턴합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK",
-                    content = @Content(schema = @Schema(implementation = AccountValidationResponse.class)))
-    })
     public ResponseEntity<AccountValidationResponse> validateNickname(String nickname){
 
         AccountValidationResponse response;
@@ -48,11 +41,6 @@ public class AccountController {
     }
 
     @PostMapping("/accounts/sign-up")
-    @Operation(summary = "회원가입", tags = "계정",
-            description = "회원가입을 진행합니다. 계정의 고유 id값(pk)을 응답합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK")
-    })
     public ResponseEntity<Long> signUp(@AuthAccount Account account,
                                        @RequestPart(value = "imageFile", required = false) MultipartFile imageFile,
                                        @Valid @RequestPart AccountSignUpRequest accountSignUpRequest) {
