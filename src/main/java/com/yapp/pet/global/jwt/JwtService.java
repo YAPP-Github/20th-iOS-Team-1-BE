a6fa9514b3f4d00d06ab7905af0243476fdd0c17
jihoon <znftm97@naver.com>
2022-06-26 20:44:24 +0900

    fix: 애플로그인 버그 수정

--- src/main/java/com/yapp/pet/global/jwt/JwtService.java
+++ src/main/java/com/yapp/pet/global/jwt/JwtService.java
@@ -1,6 +1,10 @@
 package com.yapp.pet.global.jwt;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yapp.pet.global.exception.jwt.*;
+import com.yapp.pet.web.oauth.apple.AppleClient;
+import com.yapp.pet.web.oauth.apple.model.ApplePublicKeyResponse;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -8,8 +12,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.Key;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.Base64;
 import java.util.Date;
+import java.util.Map;
 
 import static com.yapp.pet.global.TogaetherConstants.*;
 
@@ -23,11 +36,14 @@ public class JwtService {
 	private final Key key;
 	private final String issuer;
 
+	private final AppleClient appleClient;
+
 	public JwtService(@Value("${jwt.token.header-type}") String headerType,
 					  @Value("${jwt.token.issuer}") String issuer,
 					  @Value("${jwt.token.secret}") String secret,
 					  @Value("${jwt.token.access-time}") long accessTime,
-					  @Value("${jwt.token.refresh-time}") long refreshTime) {
+					  @Value("${jwt.token.refresh-time}") long refreshTime,
+					  AppleClient appleClient) {
 
 		byte[] keyBytes = Decoders.BASE64.decode(secret);
 		this.key = Keys.hmacShaKeyFor(keyBytes);
@@ -35,6 +51,7 @@ public class JwtService {
 		this.issuer = issuer;
 		this.accessTime = accessTime;
 		this.refreshTime = refreshTime;
+		this.appleClient = appleClient;
 	}
 
 	public String createAccessToken(String uniqueIdBySocial) {
@@ -94,6 +111,45 @@ public class JwtService {
 		} catch (ExpiredJwtException e) {
 			throw new ExpiredJwtTokenException();
 		}
+	}
+
+	public String getSubjectByAppleToken(String token) {
+		try {
+			ApplePublicKeyResponse response = appleClient.getApplePublicKey();
+
+			String headerOfIdentityToken = token.substring(0, token.indexOf("."));
+
+			Map<String, String> header = new ObjectMapper().readValue(
+					new String(Base64.getDecoder().decode(headerOfIdentityToken), "UTF-8"), Map.class
+			);
+
+			ApplePublicKeyResponse.Key key = response.getKeys().stream()
+					.filter(k -> k.getKid().equals(header.get("kid")) && k.getAlg().equals(header.get("alg")))
+					.findFirst().orElseThrow(() -> new NullPointerException("Failed get public key from apple's id server."));
+
+			byte[] nBytes = Base64.getUrlDecoder().decode(key.getN());
+			byte[] eBytes = Base64.getUrlDecoder().decode(key.getE());
+
+			BigInteger n = new BigInteger(1, nBytes);
+			BigInteger e = new BigInteger(1, eBytes);
+
+			RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec(n, e);
+			KeyFactory keyFactory = KeyFactory.getInstance(key.getKty());
+			PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+
+			return Jwts.parserBuilder()
+					.setSigningKey(publicKey)
+					.build()
+					.parseClaimsJws(token)
+					.getBody()
+					.getSubject();
+		} catch (ExpiredJwtException | UnsupportedEncodingException e) {
+			throw new ExpiredJwtTokenException();
+		} catch (JsonProcessingException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+
+		return "";
 	}
 
 	public void validateToken(String jwt){
