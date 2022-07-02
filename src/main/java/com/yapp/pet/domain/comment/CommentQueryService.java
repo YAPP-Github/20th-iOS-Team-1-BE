b238d2069d5b818896cf2adab8d411574ac0ac3e
java-saeng <gusdn85554@naver.com>
2022-07-02 21:06:23 +0900

    fix : 댓글 정보 반환 시 Club의 leader가 아닌 사람도 leader가 true로 반환됨
    leader를 비교 시 comment의 Account와 accountClub의 Account를 비교

--- src/main/java/com/yapp/pet/domain/comment/CommentQueryService.java
+++ src/main/java/com/yapp/pet/domain/comment/CommentQueryService.java
@@ -1,6 +1,8 @@
 package com.yapp.pet.domain.comment;
 
 import com.yapp.pet.domain.account.entity.Account;
+import com.yapp.pet.domain.pet.entity.Pet;
+import com.yapp.pet.domain.pet.repository.PetRepository;
 import com.yapp.pet.web.comment.model.CommentResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +20,8 @@ public class CommentQueryService {
 
     private final CommentRepository commentRepository;
 
+    private final PetRepository petRepository;
+
     public Comment findCommentById(long commentId) {
         return commentRepository.findById(commentId)
                                 .orElseThrow(NoSuchElementException::new);
@@ -26,14 +30,20 @@ public class CommentQueryService {
     public List<CommentResponse> findComment(long clubId, Account account) {
         List<Comment> comments = commentRepository.findCommentByClubId(clubId);
 
+        List<Pet> savedPet = petRepository.findPetsByAccountId(account.getId());
+
         return comments.stream()
                        .map(comment -> new CommentResponse(comment.getContent(),
                                                            comment.getAccount().getNickname(),
                                                            comment.getClub().getAccountClubs().stream()
                                                                   .anyMatch(ac -> (ac.isLeader() && ac.getAccount()
-                                                                                                      .equals(account))),
+                                                                                                      .equals(comment.getAccount()))),
                                                            comment.getUpdatedAt()
-                                                                  .atZone(ZoneId.of("Asia/Seoul"))))
+                                                                  .atZone(ZoneId.of("Asia/Seoul")),
+                                                           savedPet.stream().map(Pet::getBreed)
+                                                                   .collect(Collectors.toList()),
+                                                           comment.getAccount().getAccountImage().getPath()))
                        .collect(Collectors.toList());
     }
+
 }
