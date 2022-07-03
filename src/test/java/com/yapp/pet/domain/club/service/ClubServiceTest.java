7e560d7b21b13c794c89ed63ff20eb6df59588b4
jihoon <znftm97@naver.com>
2022-07-03 17:56:40 +0900

    test: 모임 삭제 테스트 코드 추가

--- src/test/java/com/yapp/pet/domain/club/service/ClubServiceTest.java
+++ src/test/java/com/yapp/pet/domain/club/service/ClubServiceTest.java
@@ -3,12 +3,14 @@ package com.yapp.pet.domain.club.service;
 import com.yapp.pet.domain.account.entity.Account;
 import com.yapp.pet.domain.account.entity.AccountSex;
 import com.yapp.pet.domain.account.repository.AccountRepository;
+import com.yapp.pet.domain.accountclub.AccountClub;
 import com.yapp.pet.domain.accountclub.AccountClubRepository;
+import com.yapp.pet.domain.club.entity.Club;
 import com.yapp.pet.domain.club.repository.ClubRepository;
+import com.yapp.pet.domain.comment.CommentRepository;
+import com.yapp.pet.global.exception.club.NotLeaderException;
 import com.yapp.pet.global.exception.club.NotParticipatingClubException;
 import com.yapp.pet.global.exception.common.ExceptionStatus;
-import com.yapp.pet.global.exception.jwt.NotFoundTokenException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -33,6 +37,9 @@ public class ClubServiceTest {
 
     @Autowired
     AccountClubRepository accountClubRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
 
     @Autowired
     ClubService clubService;
@@ -78,6 +85,42 @@ public class ClubServiceTest {
 
         //then
         assertThat(accountClubRepository.findById(2L)).isEmpty();
+    }
+
+    @Test
+    @DisplayName("모임의 방장은 모임을 삭제할 수 있다. 댓글도 함께 삭제된다.")
+    void deleteClub(){
+        //given
+        Long clubId = 1L;
+        Account loginAccount = accountRepository.findById(1L).get();
+        Club findClub = clubRepository.findById(clubId).get();
+
+        //when
+        clubService.deleteClub(clubId, loginAccount);
+
+        //then
+        assertThat(clubRepository.findById(clubId)).isEmpty();
+
+        assertThat(accountClubRepository.findAllById(
+                    findClub.getAccountClubs().stream()
+                            .map(AccountClub::getId)
+                            .collect(Collectors.toList())
+        )).isEmpty();
+
+        assertThat(commentRepository.findCommentByClubId(clubId)).isEmpty();
+    }
+
+    @Test
+    @DisplayName("모임의 방장이 아니면 모임을 삭제할 수 없다.")
+    void canNotDeleteClub(){
+        //given
+        Long clubId = 1L;
+        Account loginAccount = accountRepository.findById(2L).get();
+
+        //when & then
+        assertThatExceptionOfType(NotLeaderException.class)
+                .isThrownBy(() -> clubService.deleteClub(clubId, loginAccount))
+                .withMessageMatching(ExceptionStatus.NOT_LEADER_EXCEPTION.getMessage());
     }
 
 }
