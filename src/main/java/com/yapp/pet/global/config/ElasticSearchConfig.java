abf39752105668863adc7edc242ba893fb31427e
java-saeng <gusdn85554@naver.com>
2022-07-06 22:30:49 +0900

    feat : AccountClub -> AccountClubDocument custom converter 추가

--- src/main/java/com/yapp/pet/global/config/ElasticSearchConfig.java
+++ src/main/java/com/yapp/pet/global/config/ElasticSearchConfig.java
@@ -1,12 +1,22 @@
 package com.yapp.pet.global.config;
 
+import com.yapp.pet.domain.club.document.AccountClubDocument;
 import com.yapp.pet.domain.club.repository.ClubSearchRepository;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchCustomConversions;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackageClasses = ClubSearchRepository.class)
@@ -19,5 +29,40 @@ public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
                                                                      .connectedTo("localhost:9200")
                                                                      .build();
         return RestClients.create(clientConfiguration).rest();
+    }
+
+    @Bean
+    @Override
+    public ElasticsearchCustomConversions elasticsearchCustomConversions() {
+        return new ElasticsearchCustomConversions(
+                Arrays.asList(new AccountClubDocumentToMap(), new MapToAccountClub()));
+    }
+
+    @WritingConverter
+    static class AccountClubDocumentToMap implements Converter<AccountClubDocument, Map<String, Long>> {
+
+        @Override
+        public Map<String, Long> convert(AccountClubDocument source) {
+            Map<String, Long> target = new LinkedHashMap<>();
+
+            target.put("club", source.getClubId());
+            target.put("account", source.getAccountId());
+
+            return target;
+        }
+    }
+
+    @ReadingConverter
+    static class MapToAccountClub implements Converter<Map<String, Long>, AccountClubDocument> {
+
+        @Override
+        public AccountClubDocument convert(Map<String, Long> source) {
+
+            long accountId = ((Number) source.get("account")).longValue();
+
+            long clubId = ((Number) source.get("club")).longValue();
+
+            return AccountClubDocument.of(clubId, accountId);
+        }
     }
 }
