a9648cc50adc764dbf7c7eca9b591b5c3267bb70
java-saeng <gusdn85554@naver.com>
2022-07-14 01:01:34 +0900

    feat : Account 삭제

--- src/main/java/com/yapp/pet/domain/accountclub/AccountClubRepository.java
+++ src/main/java/com/yapp/pet/domain/accountclub/AccountClubRepository.java
@@ -1,6 +1,17 @@
 package com.yapp.pet.domain.accountclub;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface AccountClubRepository extends JpaRepository<AccountClub, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("delete from AccountClub ac where ac.account.id = :id")
+    int deleteAccountClubsByAccountId(@Param("id") long accountId);
 }
