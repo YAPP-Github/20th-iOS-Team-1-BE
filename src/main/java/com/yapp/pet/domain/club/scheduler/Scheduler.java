fe7f249f2ca948ca9b968b868b9c3ea41fee2813
java-saeng <gusdn85554@naver.com>
2022-07-25 00:24:55 +0900

    refactor : 스케줄러 10분 단위로 요청으로 변경

--- src/main/java/com/yapp/pet/domain/club/scheduler/Scheduler.java
+++ src/main/java/com/yapp/pet/domain/club/scheduler/Scheduler.java
@@ -3,6 +3,7 @@ package com.yapp.pet.domain.club.scheduler;
 import com.yapp.pet.domain.club.service.ClubQueryService;
 import com.yapp.pet.domain.club.entity.ClubStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +12,7 @@ public class Scheduler {
 
     private final ClubQueryService clubQueryService;
 
-//    @Scheduled(cron = "0 * * * * *")
+    @Scheduled(cron = "0 0/10 * * * *")
     public void exceedClub() {
         clubQueryService.exceedTimeClub()
                         .forEach(club -> {
