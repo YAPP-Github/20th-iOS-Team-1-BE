6b5977bac3a96d11584704e626097ed221008f1a
jihoon <znftm97@naver.com>
2022-07-31 00:37:12 +0900

    hotfix: 모임 종료 상태로 변경하는 스케줄러 버그 해결
    - 트랜잭션이 없어서 변경감지가 동작 하지 않음
    - 10분마다 호출되는 메서드에 트랜잭션을 걸지 않고, 실제 쿼리에만 트랜잭션을 적용

--- src/main/java/com/yapp/pet/domain/club/repository/jpa/ClubRepository.java
+++ src/main/java/com/yapp/pet/domain/club/repository/jpa/ClubRepository.java
@@ -1,10 +1,16 @@
 package com.yapp.pet.domain.club.repository.jpa;
 
 import com.yapp.pet.domain.club.entity.Club;
+import com.yapp.pet.domain.club.entity.ClubStatus;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 import java.util.Optional;
 
 public interface ClubRepository extends JpaRepository<Club, Long>, ClubRepositoryCustom {
@@ -12,6 +18,11 @@ public interface ClubRepository extends JpaRepository<Club, Long>, ClubRepositor
     @Override
     @EntityGraph(attributePaths = {"accountClubs", "eligiblePetSizeTypes", "eligibleBreeds"})
     Optional<Club> findById(Long id);
+
+    @Modifying
+    @Transactional
+    @Query("update Club c set c.status = :status where c.id in :ids")
+    void updateStatusToEndClub(@Param("ids") List<Long> ids, @Param("status") ClubStatus status);
 
     default Club findByIdWrapper(Long clubId) {
         return findById(clubId).orElseThrow(EntityNotFoundException::new);
--- src/main/java/com/yapp/pet/domain/club/scheduler/Scheduler.java
+++ src/main/java/com/yapp/pet/domain/club/scheduler/Scheduler.java
@@ -1,11 +1,17 @@
 package com.yapp.pet.domain.club.scheduler;
 
+import com.yapp.pet.domain.club.entity.Club;
 import com.yapp.pet.domain.club.entity.ClubStatus;
 import com.yapp.pet.domain.club.repository.jpa.ClubRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class Scheduler {
@@ -14,9 +20,21 @@ public class Scheduler {
 
     @Scheduled(cron = "0 0/10 * * * *")
     public void exceedClub() {
-        clubRepository.findExceedTimeClub()
-                        .forEach(club -> {
-                       club.updateStatus(ClubStatus.END);
-                   });
+        List<Club> exceedTimeClub = clubRepository.findExceedTimeClub();
+
+        if (exceedTimeClub.isEmpty()) {
+            return;
+        }
+
+        clubRepository.findExceedTimeClub().forEach(club -> {
+            log.info("모임 종료, id : {}", club.getId());
+            club.updateStatus(ClubStatus.END);
+        });
+
+        List<Long> ids = exceedTimeClub.stream()
+                .map(Club::getId)
+                .collect(Collectors.toList());
+
+        clubRepository.updateStatusToEndClub(ids, ClubStatus.END);
     }
 }
