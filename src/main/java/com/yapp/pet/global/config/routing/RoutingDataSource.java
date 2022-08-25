8bd9a85e0a02e65763a856fece65d2037c0752a2
jihoon <znftm97@naver.com>
2022-08-25 19:06:38 +0900

    fix: primary, secondary DB 설정 제거(AWS Aurora 요금 폭탄..)

--- src/main/java/com/yapp/pet/global/config/routing/RoutingDataSource.java
+++ src/main/java/com/yapp/pet/global/config/routing/RoutingDataSource.java
@@ -1,14 +1,14 @@
-package com.yapp.pet.global.config.routing;
-
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
-
-public class RoutingDataSource extends AbstractRoutingDataSource {
-
-    @Override
-    protected Object determineCurrentLookupKey() {
-        return TransactionSynchronizationManager.isCurrentTransactionReadOnly()
-                ? "secondary"
-                : "primary";
-    }
-}
+//package com.yapp.pet.global.config.routing;
+//
+//import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+//import org.springframework.transaction.support.TransactionSynchronizationManager;
+//
+//public class RoutingDataSource extends AbstractRoutingDataSource {
+//
+//    @Override
+//    protected Object determineCurrentLookupKey() {
+//        return TransactionSynchronizationManager.isCurrentTransactionReadOnly()
+//                ? "secondary"
+//                : "primary";
+//    }
+//}
